010cf5fe336a665664c13ece722975a892808029
Aaditya Singhal <aadityasinghal77@gmail.com>
2026-02-20 12:01:27 +0530

    objects in dart

--- Flutter/game/test/widget_test.dart
+++ Flutter/game/test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:game/main.dart';
-
+  
 void main() {
   testWidgets('Flappy Bird game smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
